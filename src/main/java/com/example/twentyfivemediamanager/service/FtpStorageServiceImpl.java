4f46072da285cdef7e5b4f63eb60eda9420706c1
StefanoMiceli90 <stefano.miceli@ext.leroymerlin.it>
2024-05-20 17:12:32 +0200

    Bug fixing

--- src/main/java/com/example/twentyfivemediamanager/service/FtpStorageServiceImpl.java
+++ src/main/java/com/example/twentyfivemediamanager/service/FtpStorageServiceImpl.java
@@ -56,6 +56,7 @@ public class FtpStorageServiceImpl implements FileStorageService {
         for (int i = 0; i < directory.length; i ++){
             path.append("/").append(directory[i]);
         }
+        path.append(file.getOriginalFilename());
         try {
             if (file.isEmpty()) {
                 throw new RuntimeException("Failed to store empty file.");
